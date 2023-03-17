c5dd1a33ec4c3de318b869cb1bb3396411ba28c7
samuelebumbello <samuelebumbello17@gmail.com>
2023-03-17 14:03:06 +0100

    first commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+/* ************************************************************
+
+1- Chiedi all’utente il suo nome,
+
+2- poi chiedi il suo cognome,
+
+3- poi chiedi il suo colore preferito
+
+4- Infine scrivi sulla pagina nomecognomecolorepreferito23
+
+**************************************************************/
