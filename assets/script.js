7db28b62f16bfe44658bf27a6b1715885dcfed13
samuelebumbello <samuelebumbello17@gmail.com>
2023-03-20 00:38:34 +0100

    last commit

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,22 @@
 4- Infine scrivi sulla pagina nomecognomecolorepreferito23
 
 **************************************************************/
+
+const name = parseInt(prompt ("Inserisci il tuo nome"))
+console.log(name)
+
+const surname = parseInt(prompt ("Inserisci il tuo cognome"))
+console.log(surname)
+
+const color = parseInt(prompt("Inserisci il tuo colore preferito"))
+console.log(color)
+
+const outputStr = `
+nome ${name}
+cognome ${surname}
+colorepreferito23 ${color}
+`
+console.log(outputStr)
+
+document.getElementById("output").innerHTML = outputStr;
+
